c4eba4924bb7901526101ab57e8a282cf9bc671d
Alexander <san.juckov2017@yandex.ru>
2024-05-23 22:28:54 +0300

    Adding a new model to the users application, as well as recording data through the fixtures tool and adding a custom command

---
+++ users/management/commands/load_payments.py
@@ -0,0 +1,58 @@
+import json
+from django.core.management.base import BaseCommand
+from users.models import Payment, User
+from materials.models import Course, Lesson
+
+
+class Command(BaseCommand):
+    help = 'Load payments from a JSON file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('json_file', type=str, help='The path to the JSON file containing the payment data')
+
+    def handle(self, *args, **kwargs):
+        json_file = kwargs['json_file']
+
+        with open(json_file, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+            for entry in data:
+                fields = entry['fields']
+                user_id = fields['user']
+                self.stdout.write(f'Processing payment for user ID: {user_id}')
+
+                try:
+                    user = User.objects.get(pk=user_id)
+                except User.DoesNotExist:
+                    self.stdout.write(self.style.ERROR(f'User with ID {user_id} does not exist'))
+                    continue
+
+                paid_course_id = fields.get('paid_course')
+                paid_course = None
+                if paid_course_id:
+                    try:
+                        paid_course = Course.objects.get(pk=paid_course_id)
+                    except Course.DoesNotExist:
+                        self.stdout.write(self.style.ERROR(f'Course with ID {paid_course_id} does not exist'))
+                        continue
+
+                paid_lesson_id = fields.get('paid_lesson')
+                paid_lesson = None
+                if paid_lesson_id:
+                    try:
+                        paid_lesson = Lesson.objects.get(pk=paid_lesson_id)
+                    except Lesson.DoesNotExist:
+                        self.stdout.write(self.style.ERROR(f'Lesson with ID {paid_lesson_id} does not exist'))
+                        continue
+
+                payment = Payment(
+                    user=user,
+                    payment_date=fields['payment_date'],
+                    paid_course=paid_course,
+                    paid_lesson=paid_lesson,
+                    amount=fields['amount'],
+                    payment_method=fields['payment_method']
+                )
+                payment.save()
+
+        self.stdout.write(self.style.SUCCESS('Successfully loaded payments from %s' % json_file))
--- users/models.py
+++ users/models.py
@@ -1,5 +1,29 @@
-from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
+from materials.models import Course, Lesson
+
+from django.contrib.auth.models import AbstractUser
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счет'),
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата оплаты')
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Оплаченный курс')
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Оплаченный урок')
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHOD_CHOICES, verbose_name='Способ оплаты')
+
+    def __str__(self):
+        return f'{self.user} - {self.amount}'
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
 
 
 class User(AbstractUser):
