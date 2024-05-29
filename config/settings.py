e1ea06c4469213fd2eec239449edea081f58d8e6
Alexander <san.juckov2017@yandex.ru>
2024-05-29 13:04:42 +0300

    Implementation of CRUD for users, including user registration, setting up the use of JWT authorization in the project.

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_filters',
+    'rest_framework_simplejwt',
 
     'users',
     'materials',
@@ -78,6 +80,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 
 # Database
@@ -140,3 +149,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
--- users/models.py
+++ users/models.py
@@ -11,12 +11,35 @@ class Payment(models.Model):
         ('transfer', 'Перевод на счет'),
     ]
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
-    payment_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата оплаты')
-    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Оплаченный курс')
-    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Оплаченный урок')
-    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
-    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHOD_CHOICES, verbose_name='Способ оплаты')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    payment_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата оплаты'
+    )
+    paid_course = models.ForeignKey(
+        Course, on_delete=models.CASCADE,
+        null=True, blank=True,
+        verbose_name='Оплаченный курс'
+    )
+    paid_lesson = models.ForeignKey(
+        Lesson, on_delete=models.CASCADE,
+        null=True, blank=True,
+        verbose_name='Оплаченный урок'
+    )
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Сумма оплаты'
+    )
+    payment_method = models.CharField(
+        max_length=10,
+        choices=PAYMENT_METHOD_CHOICES,
+        verbose_name='Способ оплаты'
+    )
 
     def __str__(self):
         return f'{self.user} - {self.amount}'
@@ -29,12 +52,26 @@ class Payment(models.Model):
 class User(AbstractUser):
     username = None
 
-    email = models.EmailField(unique=True, verbose_name="Почта", help_text="Укажите почту")
-    phone = models.CharField(max_length=35, blank=True, null=True, verbose_name="Телефон", help_text="Укажите номер"
-                                                                                                     " телефона")
-    city = models.CharField(max_length=50, verbose_name="Город", help_text="Укажите город")
-    avatar = models.ImageField(upload_to="users/avatars", blank=True, null=True, verbose_name="Аватарка",
-                               help_text="Загрузите аватарку")
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=35,
+        blank=True, null=True,
+        verbose_name="Телефон",
+        help_text="Укажите номер телефона"
+    )
+    city = models.CharField(
+        max_length=50,
+        verbose_name="Город",
+        help_text="Укажите город"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        blank=True, null=True,
+        verbose_name="Аватарка",
+        help_text="Загрузите аватарку"
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
