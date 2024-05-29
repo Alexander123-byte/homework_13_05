2288dfde3465c1e1abb935a3b45bfd67e9ef1ee0
Alexander <san.juckov2017@yandex.ru>
2024-05-29 14:50:41 +0300

    Closing each endpoint with authorization

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
@@ -20,6 +21,7 @@ class PaymentViewSet(ModelViewSet):
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
