d9fea44ea9f97f632fdc86ebc4aa6a50648691db
Alexander <san.juckov2017@yandex.ru>
2024-05-29 14:39:14 +0300

    Updating file's

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from rest_framework.serializers import ModelSerializer
 
 from users.models import User
@@ -7,35 +6,4 @@ from users.models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
-=======
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'phone', 'city', 'avatar']
-
-
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['email', 'phone', 'city', 'avatar', 'password']
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            email=validated_data['email'],
-            phone=validated_data['phone'],
-            city=validated_data['city'],
-            avatar=validated_data['avatar']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
->>>>>>> origin/main
+        fields = ('email', 'phone', 'city', 'avatar')
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
+from .apps import UsersConfig
 from .views import PaymentViewSet
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from users.views import UserCreateAPIView
+
+app_name = UsersConfig.name
 
 router = DefaultRouter()
 router.register(r'payments', PaymentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
